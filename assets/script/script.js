fa3b9c033afb19d735b8513eddf7e13be1846843
James Price <james.a.t.price@gmail.com>
2024-02-09 12:46:36 +0000

    Fixed questions not appearing.

--- assets/script/script.js
+++ assets/script/script.js
@@ -18,7 +18,7 @@ const leaderboardMedium = document.getElementById('leaderboard-medium');
 const leaderboardHard = document.getElementById('leaderboard-hard');
 const leaderboardRandom = document.getElementById('leaderboard-classic');
 const difficultyContainer = document.getElementById('difficulty-selector-container');
-const instructions = document.getElementById('instructions');
+const instructions = document.getElementById('instructions-btn');
 const difficultyLabel = document.getElementById('diff-select-label');
 let playerNameInput = document.getElementById('player-name-submit');
 let playerName = '';
@@ -386,7 +386,7 @@ function updateLeaderboardView() {
 function startGame() {
     if (window.location.pathname !== '/index.html') {
         // Redirect to index.html
-        window.location.href = '/index.html';
+        window.location.href = 'movie-quiz/index.html';
         return; // Stop further execution of the function
     }
     
